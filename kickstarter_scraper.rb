4a332f1d0b55840c53d8621c9f57e9eb92c70552
Bryce B <bryce.boddy@gmail.com>
2020-05-07 06:47:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,4 +9,17 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 end
 
-create_project_hash
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+ 
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+ 
+  # return the projects hash
+  projects
+end
